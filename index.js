710bc866161e5d11812a132c487b8533b6623cb4
Christoph Witzko <github@christophwitzko.com>
2015-02-26 00:53:12 +0100

    feat: added socket.io

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+var http = require('http')
+
 var debug = require('debug')('wat:index')
 var express = require('express')
+
 var app = express()
+var server = http.Server(app)
+var io = require('socket.io')(server)
 
 app.get('/', function (req, res) {
   res.send('WAT?')
 })
 
-app.listen(5000, function () {
+io.on('connection', function (socket) {
+  console.log('a user connected')
+  socket.on('disconnect', function () {
+    console.log('user disconnected')
+  })
+})
+
+server.listen(5000, function () {
   debug('Listening on port %d', 5000)
 })
